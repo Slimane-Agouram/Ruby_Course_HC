3b72f2df76ec41d95bf969def19846fb1ffeb041
Slimane-Agouram <slimane.agouram@gmail.com>
2017-07-03 11:54:52 +0200

    version for

--- Ruby_Course_Day_1/ruby_game.rb
+++ Ruby_Course_Day_1/ruby_game.rb
@@ -1,16 +1,11 @@
 nb_cols=5
 nb_rows=5
 
-index_2=0
-
-
-while index_2<nb_rows
-index_1=0
-while index_1<nb_cols
-    print "."
-    index_1 +=1
+for index_row in 1..nb_rows
+for index in 1..nb_cols
+print "."
 end
-puts " "
-index_2 = index_2 + 1
+puts ""
 end
+
 
