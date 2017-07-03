de7baaeba31e5a56682dfacb5700ea89d0895d6d
Slimane-Agouram <slimane.agouram@gmail.com>
2017-07-03 11:59:12 +0200

    version for

--- Ruby_Course_Day_1/ruby_game.rb
+++ Ruby_Course_Day_1/ruby_game.rb
@@ -1,11 +1,14 @@
 nb_cols=5
 nb_rows=5
 
+#first loop
 for index_row in 1..nb_rows
-for index in 1..nb_cols
-print "."
-end
-puts ""
+    #second loop
+    for index in 1..nb_cols
+        print "."
+    end
+    #back to line at the end
+    puts ""
 end
 
 
