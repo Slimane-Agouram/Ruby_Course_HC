2d58165f48e9e9c008554b57002f20291666a33f
Slimane-Agouram <slimane.agouram@gmail.com>
2017-07-03 12:21:38 +0200

    version if

--- Ruby_Course_Day_1/ruby_game.rb
+++ Ruby_Course_Day_1/ruby_game.rb
@@ -1,18 +1,24 @@
 nb_cols=5
 nb_rows=5
 
+player_col=3
+player_row=2
+
+
 # first loop for rows
 for index_row in 1..nb_rows
     # second loop for columns
     for index in 1..nb_cols
 
         # check for ending column
-        if index != nb_cols
-            print '.'
+        if  index==player_col && index_row==player_row
+            print 'X'
         else
-            puts '.'
+            print '.'
         end
+
     end
+    puts ''
 end
 
 
