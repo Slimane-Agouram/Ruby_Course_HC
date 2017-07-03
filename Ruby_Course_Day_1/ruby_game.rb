83b49a4c049725bd28f9ba87bfd7905a5773bf89
Slimane-Agouram <slimane.agouram@gmail.com>
2017-07-03 15:15:10 +0200

    version if

--- Ruby_Course_Day_1/ruby_game.rb
+++ Ruby_Course_Day_1/ruby_game.rb
@@ -1,24 +1,27 @@
-nb_cols=5
-nb_rows=5
+
 
 player = {col: 2, row: 3}
 
+def draw(nb_col, nb_row, player)
+    # first loop for rows
+    for index_row in 1..nb_row
+        # second loop for columns
+        for index_col in 1..nb_col
 
+            # check for ending column
+            if  index_col==player[:col]&& index_row==player[:row]
+                print 'X'
+            else
+                print '.'
+            end
 
-# first loop for rows
-for index_row in 1..nb_rows
-    # second loop for columns
-    for index in 1..nb_cols
-
-        # check for ending column
-        if  index==player[:col]&& index_row==player[:row]
-            print 'X'
-        else
-            print '.'
         end
-
+        puts ''
     end
     puts ''
 end
+
+draw(5,6,player)
+draw(2,2,player)
 
 
