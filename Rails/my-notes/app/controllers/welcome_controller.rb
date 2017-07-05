daf695172bf99d70c0d2250cb290526f08038e05
Slimane-Agouram <slimane.agouram@gmail.com>
2017-07-05 10:46:12 +0200

    Web manipulation

--- Rails/my-notes/app/controllers/welcome_controller.rb
+++ Rails/my-notes/app/controllers/welcome_controller.rb
@@ -2,4 +2,14 @@ class WelcomeController < ApplicationController
     def hello
         @message=params[:message]
     end
+
+    def reponse
+        puts 'json==========='
+        render json:{toto:'toto'}
+    end
+
+    def goodbye
+        puts 'goodbye==========='
+        render "goodbye"
+    end
 end
