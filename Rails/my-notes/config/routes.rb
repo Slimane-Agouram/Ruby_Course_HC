b0b5f7023fca172435751eb58e5b263423f57725
Slimane-Agouram <slimane.agouram@gmail.com>
2017-07-05 10:49:51 +0200

    between brackets to make params complementary

--- Rails/my-notes/config/routes.rb
+++ Rails/my-notes/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root'welcome#hello'
    #get "welcome/hello",to: "welcome#hello"
-   get "welcome/hello/:message", to: "welcome#reponse"
+   get "welcome/hello(/:message)", to: "welcome#reponse"
 
    get '/goodbye', to: "welcome#goodbye"
 
